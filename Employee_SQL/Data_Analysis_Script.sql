d44f43ad6f5a9c5dc5ac8511a4ae38cd12efcc1f
JessieJay7 <giftywdappiagyei@gmail.com>
2024-06-30 20:24:00 -0500

    Learning Sequel

--- Employee_SQL/Data_Analysis_Script.sql
+++ Employee_SQL/Data_Analysis_Script.sql
@@ -3,3 +3,7 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM Employees e
 JOIN Salaries s ON e.emp_no = s.emp_no;
 
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM Employees
+WHERE hire_date LIKE '%1986';
