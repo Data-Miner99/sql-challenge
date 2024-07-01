cf0d8d5f9dcdab8310c1859758a77594c0d047e8
JessieJay7 <giftywdappiagyei@gmail.com>
2024-06-30 20:19:57 -0500

    Updated Schema

--- Employee_SQL/employees_schema.sql
+++ Employee_SQL/employees_schema.sql
@@ -1,52 +1,52 @@
 -- CREATE A DEPARTMENTS TABLE.
-CREATE TABLE "Departments" (
-    "dept_no" VARCHAR NOT NULL,
-    "dept_name" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY ("dept_no")
+CREATE TABLE Departments (
+    dept_no VARCHAR NOT NULL,
+    dept_name VARCHAR NOT NULL,
+    PRIMARY KEY (dept_no)
 );
 
 -- CREATE A TITLES TABLE.
-CREATE TABLE "Titles" (
-    "title_id" VARCHAR NOT NULL,
-    "title" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY ("title_id")
+CREATE TABLE Titles (
+    title_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    PRIMARY KEY (title_id)
 );
 
 -- CREATE AN EMPLOYEES TABLE.
-CREATE TABLE "Employees" (
-    "emp_no" INT NOT NULL,
-    "emp_title_id" VARCHAR NOT NULL,
-    "birth_date" VARCHAR NOT NULL,
-    "first_name" VARCHAR NOT NULL,
-    "last_name" VARCHAR NOT NULL,
-    "sex" VARCHAR NOT NULL,
-    "hire_date" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY ("emp_no"),
-    CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY ("emp_title_id") REFERENCES "Titles" ("title_id")
+CREATE TABLE Employees (
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR NOT NULL,
+    hire_date VARCHAR NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES Titles (title_id)
 );
 
 -- CREATE A DEPARTMENT EMPLOYEES TABLE.
-CREATE TABLE "Dept_emp" (
-    "emp_no" INT NOT NULL,
-    "dept_no" VARCHAR NOT NULL,
-    CONSTRAINT "pk_Dept_emp" PRIMARY KEY ("emp_no", "dept_no"),
-    CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY ("emp_no") REFERENCES "Employees" ("emp_no"),
-    CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY ("dept_no") REFERENCES "Departments" ("dept_no")
+CREATE TABLE Dept_emp (
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
 );
 
 -- CREATE A MANAGERS TABLE.
-CREATE TABLE "Dept_manager" (
-    "dept_no" VARCHAR NOT NULL,
-    "emp_no" INT NOT NULL,
-    CONSTRAINT "pk_Dept_manager" PRIMARY KEY ("dept_no", "emp_no"),
-    CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY ("dept_no") REFERENCES "Departments" ("dept_no"),
-    CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY ("emp_no") REFERENCES "Employees" ("emp_no")
+CREATE TABLE Dept_manager (
+    dept_no VARCHAR NOT NULL,
+    emp_no INTEGER NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
 );
 
 -- CREATE A SALARIES TABLE.
-CREATE TABLE "Salaries" (
-    "emp_no" INT NOT NULL,
-    "salary" INT NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY ("emp_no"),
-    CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY ("emp_no") REFERENCES "Employees" ("emp_no")
+CREATE TABLE Salaries (
+    emp_no INTEGER NOT NULL,
+    salary INTEGER NOT NULL,
+    PRIMARY KEY ("emp_no"),
+    FOREIGN KEY ("emp_no") REFERENCES Employees (emp_no)
 );
