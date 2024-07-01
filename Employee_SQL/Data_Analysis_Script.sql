cbd1ff3e740f9e43fa33b17ff95be7c183fd777f
JessieJay7 <giftywdappiagyei@gmail.com>
2024-06-30 23:46:07 -0500

    Learning SQL is fun!

--- Employee_SQL/Data_Analysis_Script.sql
+++ Employee_SQL/Data_Analysis_Script.sql
@@ -31,3 +31,17 @@ FROM Employees e
 JOIN Dept_emp de ON e.emp_no =de.emp_no
 JOIN Departments d ON de.dept_no = d.dept_no
 WHERE d.dept_no = 'd007';
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Employees e
+JOIN Dept_emp de ON e.emp_no = de.emp_no
+JOIN Departments d ON de.dept_no = d.dept_no
+WHERE d.dept_no = 'd005' OR d.dept_no = 'd007';
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name,
+COUNT(last_name) AS "Frequency Count"
+FROM Employees
+GROUP BY last_name
+ORDER BY "Frequency Count" DESC;
