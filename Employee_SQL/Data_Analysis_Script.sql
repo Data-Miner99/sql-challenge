3b0a71caa7daec11fc85e7c4868ab7d5f37355b6
JessieJay7 <giftywdappiagyei@gmail.com>
2024-07-04 13:44:14 -0500

    Updated Script

--- Employee_SQL/Data_Analysis_Script.sql
+++ Employee_SQL/Data_Analysis_Script.sql
@@ -6,7 +6,7 @@ JOIN Salaries s ON e.emp_no = s.emp_no;
 --List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM Employees
-WHERE hire_date LIKE '%1986';
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
@@ -30,14 +30,14 @@ SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM Employees e
 JOIN Dept_emp de ON e.emp_no =de.emp_no
 JOIN Departments d ON de.dept_no = d.dept_no
-WHERE d.dept_no = 'd007';
+WHERE d.dept_name = 'Sales';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM Employees e
 JOIN Dept_emp de ON e.emp_no = de.emp_no
 JOIN Departments d ON de.dept_no = d.dept_no
-WHERE d.dept_no = 'd005' OR d.dept_no = 'd007';
+WHERE d.dept_name IN ('Sales', 'Development');
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name,
