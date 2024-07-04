49f2d08984f62465ee1d85a1d63c6ef5d37e932f
JessieJay7 <giftywdappiagyei@gmail.com>
2024-07-04 13:07:39 -0500

    Processed employee data

--- Employee_SQL/employees_schema.sql
+++ Employee_SQL/employees_schema.sql
@@ -1,26 +1,26 @@
 -- CREATE A DEPARTMENTS TABLE.
 CREATE TABLE Departments (
-    dept_no VARCHAR NOT NULL,
-    dept_name VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(40) NOT NULL,
     PRIMARY KEY (dept_no)
 );
 
 -- CREATE A TITLES TABLE.
 CREATE TABLE Titles (
-    title_id VARCHAR NOT NULL,
-    title VARCHAR NOT NULL,
+    title_id VARCHAR(5) NOT NULL,
+    title VARCHAR(40) NOT NULL,
     PRIMARY KEY (title_id)
 );
 
 -- CREATE AN EMPLOYEES TABLE.
 CREATE TABLE Employees (
-    emp_no INT NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
-    birth_date VARCHAR NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
-    hire_date VARCHAR NOT NULL,
+    emp_no INTEGER(10) NOT NULL,
+    emp_title_id VARCHAR(40) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    sex VARCHAR(4) NOT NULL,
+    hire_date DATE NOT NULL,
     PRIMARY KEY (emp_no),
     FOREIGN KEY (emp_title_id) REFERENCES Titles (title_id)
 );
@@ -28,7 +28,7 @@ CREATE TABLE Employees (
 -- CREATE A DEPARTMENT EMPLOYEES TABLE.
 CREATE TABLE Dept_emp (
     emp_no INTEGER NOT NULL,
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
@@ -36,7 +36,7 @@ CREATE TABLE Dept_emp (
 
 -- CREATE A MANAGERS TABLE.
 CREATE TABLE Dept_manager (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INTEGER NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
